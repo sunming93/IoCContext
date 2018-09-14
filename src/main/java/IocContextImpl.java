99d811dfdf963629b9fcd12976114acd9b07532f
mingwhsun <mingwhsun@thoughtworks.com>
2018-09-14 14:46:03 +0800

    [sunming] Add IocContextImpl

---
+++ src/main/java/IocContextImpl.java
@@ -0,0 +1,11 @@
+public class IocContextImpl implements IocContext {
+    @Override
+    public void registerBean(Class<?> beanClazz) {
+
+    }
+
+    @Override
+    public <T> T getBean(Class<T> resolveClazz) {
+        return null;
+    }
+}
