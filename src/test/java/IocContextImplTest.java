83d468fd41fd6318af98739a94977e6d18ee5e8a
mingwhsun <mingwhsun@thoughtworks.com>
2018-09-16 21:06:33 +0800

    [sunming] Add the test should_call_the_close_of_beanClass_in_opposite_order_when_IoCContext_call_the_close and pass it

--- src/test/java/IocContextImplTest.java
+++ src/test/java/IocContextImplTest.java
@@ -17,14 +17,14 @@ class IocContextImplTest {
     void should_throw_exception_when_create_instance_for_null() {
         Throwable exception = assertThrows(IllegalArgumentException.class,
                 () -> context.registerBean(null));
-        assertEquals("beanClazz is mandatory.",exception.getMessage());
+        assertEquals("beanClazz is mandatory.", exception.getMessage());
     }
 
     @Test
     void should_throw_exception_if_instance_cannot_be_created() {
         Throwable exception = assertThrows(IllegalArgumentException.class,
                 () -> context.registerBean(AbstractBean.class));
-        assertEquals("beans.AbstractBean is abstract.",exception.getMessage());
+        assertEquals("beans.AbstractBean is abstract.", exception.getMessage());
     }
 
 
@@ -32,7 +32,7 @@ class IocContextImplTest {
     void should_throw_exception_if_register_a_class_without_default_constructor() {
         Throwable exception = assertThrows(IllegalArgumentException.class,
                 () -> context.registerBean(NoDefaultBean.class));
-        assertEquals("beans.NoDefaultBean has no default constructor.",exception.getMessage());
+        assertEquals("beans.NoDefaultBean has no default constructor.", exception.getMessage());
     }
 
     @Test
@@ -45,14 +45,14 @@ class IocContextImplTest {
     void should_throw_exception_if_resolveClazz_is_null() {
         Throwable exception = assertThrows(IllegalArgumentException.class,
                 () -> context.getBean(null));
-        assertEquals("resolveClass is null.",exception.getMessage());
+        assertEquals("resolveClass is null.", exception.getMessage());
     }
 
     @Test
     void should_throw_exception_if_resolveClazz_has_not_been_registered() {
         Throwable exception = assertThrows(IllegalStateException.class,
                 () -> context.getBean(MyBean.class));
-        assertEquals("the class has not been registered.",exception.getMessage());
+        assertEquals("the class has not been registered.", exception.getMessage());
     }
 
     @Test
@@ -61,51 +61,51 @@ class IocContextImplTest {
 
         Throwable exception = assertThrows(MyException.class,
                 () -> context.getBean(ExceptionBean.class));
-        assertEquals("the constructor throws an exception.",exception.getMessage());
+        assertEquals("the constructor throws an exception.", exception.getMessage());
     }
 
     @Test
-    void cannot_be_registered_if_getBean() throws MyException{
+    void cannot_be_registered_if_getBean() throws MyException {
         context.registerBean(MyBean.class);
         context.getBean(MyBean.class);
 
         Throwable exception = assertThrows(IllegalStateException.class,
                 () -> context.registerBean(AnotherBean.class));
-        assertEquals("cannot be registered.",exception.getMessage());
+        assertEquals("cannot be registered.", exception.getMessage());
     }
 
     @Test
-    void can_be_get_if_registered() throws MyException{
+    void can_be_get_if_registered() throws MyException {
         context.registerBean(MyBean.class);
         MyBean oneInstance = context.getBean(MyBean.class);
 
-        assertEquals(MyBean.class,oneInstance.getClass());
+        assertEquals(MyBean.class, oneInstance.getClass());
     }
 
     @Test
-    void can_be_get_if_registered_two_class() throws MyException{
+    void can_be_get_if_registered_two_class() throws MyException {
         context.registerBean(MyBean.class);
         context.registerBean(AnotherBean.class);
 
         MyBean myBean = context.getBean(MyBean.class);
         AnotherBean anotherBean = context.getBean(AnotherBean.class);
 
-        assertEquals(MyBean.class,myBean.getClass());
-        assertEquals(AnotherBean.class,anotherBean.getClass());
+        assertEquals(MyBean.class, myBean.getClass());
+        assertEquals(AnotherBean.class, anotherBean.getClass());
     }
 
     @Test
-    void should_get_different_instance_if_get_two_times() throws MyException{
+    void should_get_different_instance_if_get_two_times() throws MyException {
         context.registerBean(MyBean.class);
 
         MyBean oneInstance = context.getBean(MyBean.class);
         MyBean anotherInstance = context.getBean(MyBean.class);
 
-        assertNotSame(oneInstance,anotherInstance);
+        assertNotSame(oneInstance, anotherInstance);
     }
 
     @Test
-    void should_create_an_instance_of_the_son_class_if_get_by_the_super_class() throws MyException{
+    void should_create_an_instance_of_the_son_class_if_get_by_the_super_class() throws MyException {
         context.registerBean(MyBeanBase.class, MyBean.class);
 
         MyBeanBase myBeanBase = context.getBean(MyBeanBase.class);
@@ -114,7 +114,7 @@ class IocContextImplTest {
     }
 
     @Test
-    void should_create_an_instance_of_the_son_class_if_get_by_the_son_class() throws MyException{
+    void should_create_an_instance_of_the_son_class_if_get_by_the_son_class() throws MyException {
         context.registerBean(MyBeanBase.class, MyBean.class);
 
         MyBeanBase myBeanBase = context.getBean(MyBean.class);
@@ -123,7 +123,7 @@ class IocContextImplTest {
     }
 
     @Test
-    void should_create_an_instance_of_the_implement_class_if_get_by_the_interface() throws MyException{
+    void should_create_an_instance_of_the_implement_class_if_get_by_the_interface() throws MyException {
         context.registerBean(MyBeanInterface.class, MyBean.class);
 
         MyBeanInterface myBeanInterface = context.getBean(MyBeanInterface.class);
@@ -132,7 +132,7 @@ class IocContextImplTest {
     }
 
     @Test
-    void should_create_an_instance_of_the_implement_class_if_get_by_the_implement_class() throws MyException{
+    void should_create_an_instance_of_the_implement_class_if_get_by_the_implement_class() throws MyException {
         context.registerBean(MyBeanInterface.class, MyBean.class);
 
         MyBeanInterface myBeanInterface = context.getBean(MyBean.class);
@@ -141,38 +141,38 @@ class IocContextImplTest {
     }
 
     @Test
-    void should_cover_the_previous_registered_type() throws MyException{
+    void should_cover_the_previous_registered_type() throws MyException {
         context.registerBean(MyBeanBase.class, MyBean.class);
         context.registerBean(MyBeanBase.class, MyBeanCooler.class);
 
         MyBeanBase myBeanBase = context.getBean(MyBeanBase.class);
 
-        assertEquals(myBeanBase.getClass(),MyBeanCooler.class);
+        assertEquals(myBeanBase.getClass(), MyBeanCooler.class);
     }
 
     @Test
-    void should_cover_the_previous_registered_type2() throws MyException{
+    void should_cover_the_previous_registered_type2() throws MyException {
         context.registerBean(MyBeanBase.class, MyBeanCooler.class);
         context.registerBean(MyBeanBase.class, MyBean.class);
 
         MyBeanBase myBeanBase = context.getBean(MyBeanBase.class);
 
-        assertEquals(myBeanBase.getClass(),MyBean.class);
+        assertEquals(myBeanBase.getClass(), MyBean.class);
     }
 
     @Test
-    void should_an_instance_extends_the_super_class_and_implements_the_interface() throws MyException{
+    void should_an_instance_extends_the_super_class_and_implements_the_interface() throws MyException {
         context.registerBean(MyBeanBase.class, MyBean.class);
         context.registerBean(MyBeanInterface.class, MyBean.class);
 
         MyBean myBean = context.getBean(MyBean.class);
 
-        assertEquals(myBean.getClass().getSuperclass(),MyBeanBase.class);
-        assertEquals(myBean.getClass().getInterfaces()[0],MyBeanInterface.class);
+        assertEquals(myBean.getClass().getSuperclass(), MyBeanBase.class);
+        assertEquals(myBean.getClass().getInterfaces()[0], MyBeanInterface.class);
     }
 
     @Test
-    void should_create_an_instance_and_dependency_and_assign_to_field() throws MyException{
+    void should_create_an_instance_and_dependency_and_assign_to_field() throws MyException {
         context.registerBean(MyBeanWithDependency.class);
         context.registerBean(MyDependency.class);
 
@@ -187,11 +187,11 @@ class IocContextImplTest {
 
         Throwable exception = assertThrows(IllegalStateException.class,
                 () -> context.getBean(MyBeanWithDependency.class));
-        assertEquals("beans.MyDependency has not been registered.",exception.getMessage());
+        assertEquals("beans.MyDependency has not been registered.", exception.getMessage());
     }
 
     @Test
-    void should_create_the_field_from_super_class_to_son_class() throws MyException{
+    void should_create_the_field_from_super_class_to_son_class() throws MyException {
         context.registerBean(MyBeanSonWithDependency.class);
         context.registerBean(MyDependency.class);
 
@@ -216,7 +216,7 @@ class IocContextImplTest {
     }
 
     @Test
-    void should_call_the_close_of_beanClass_when_IoCContext_call_the_close() throws Exception{
+    void should_call_the_close_of_beanClass_when_IoCContext_call_the_close() throws Exception {
         context.registerBean(MyBeanWithAutoCloseable.class);
 
         context.getBean(MyBeanWithAutoCloseable.class);
@@ -226,4 +226,16 @@ class IocContextImplTest {
                 IocContext.closeMethods.toArray());
     }
 
+    @Test
+    void should_call_the_close_of_beanClass_in_opposite_order_when_IoCContext_call_the_close() throws Exception {
+        context.registerBean(MyBeanWithAutoCloseable.class);
+        context.registerBean(AnotherBeanWithAutoCloseable.class);
+
+        context.getBean(MyBeanWithAutoCloseable.class);
+        context.getBean(AnotherBeanWithAutoCloseable.class);
+        context.close();
+
+        assertArrayEquals(new String[]{"beans.AnotherBeanWithAutoCloseable","beans.MyBeanWithAutoCloseable"},
+                IocContext.closeMethods.toArray());
+    }
 }
